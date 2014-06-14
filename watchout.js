9c976a57fc035486d2e49fa4f0c9a7dabc48c23b
Jonathan Bird <jonbird81@gmail.com>
2014-06-13 22:47:15 -0500

    Player on board

--- watchout.js
+++ watchout.js
@@ -58,7 +58,84 @@ var renderEnemies = function (enemy_data){
   
 }
 
+// Establish a shape for a player
+// Keep track of the player
+var Player = function () {
+//    variables of state
+  this.path = 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z';
+  this.fill = 'red';
+  // this.x = 0;
+  // this.y = 0;
+  this.x = boardSettings.width * 0.5,
+  this.y = boardSettings.height * 0.5
+  this.angle = 0;
+}
 
-// 
-renderEnemies(createEnemies());
-console.log(createEnemies());
+//    render a path in svg
+Player.prototype.render = function (to) {
+  var el = to.append('svg:path')
+             .attr('d', this.path)
+             .attr('fill', this.fill);
+//    initially render player in middle of board
+  var transform = {
+    x: boardSettings.width * 0.5,
+    y: boardSettings.height * 0.5
+  };
+//    make draggable by mouse
+  //this.setupDragging();
+  return this;
+};
+
+var player = new Player();
+player.render(board);
+renderEnemies(createEnemies);
+// Player.prototype.get = function(c) {
+//   return this[c];
+// };
+
+// Player.prototype.setX = function() {
+//   var minX = boardSettings.padding;
+//   var maxX = boardSettings.width - boardSettings.padding;  
+//   if (this.x <= minX) {
+//     this.x = minX;
+//   }
+//   if (this.x >= maxX) {
+//     this.x = maxX;
+//   }
+// };
+
+// Player.prototype.setY = function() {
+//   var minY = boardSettings.padding;
+//   var maxY = boardSettings.height - boardSettings.padding;  
+//   if (this.y <= minY) {
+//     this.y = minY;
+//   }
+//   if (this.y >= maxY) {
+//     this. = maxY;
+//   }
+// }
+
+// Player.prototype.transform = function (options) {
+//   this.angle = options.angle || this.angle;
+//   this.setX(options.x || this.x);
+//   this.setY(options.y || this.y);
+// };
+
+//    restrict player's movement to a particular
+
+//    add methods to move the player
+//      translation (x1,y1 to x2,y2)
+//      rotate player to face direction of translation
+//  make a new player object and render to gameboard
+
+
+// Create a turn function, invoke that periodically
+//  Turn function invokes the render 
+// var turn = function(){
+//   renderEnemies(createEnemies());
+
+// };
+
+// setInterval(function(){
+//   turn();
+// }, 100);
