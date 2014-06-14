51703ff7c645f1ae3d013ef04d6c791ff04ce096
Jonathan Bird <jonbird81@gmail.com>
2014-06-13 19:11:03 -0500

    Added board setting, created axes and initialized board

--- watchout.js
+++ watchout.js
@@ -1,2 +1,26 @@
 // start slingin' some d3 here.
-
+// Create the gameboard
+//    Add html element to body for containing gameboard
+var board = d3.select('body');
+//    Add css to position containing element?
+//    Settings for the height, width, set number of enemies (random)
+var boardSettings = {
+  height: 450,
+  width: 700,
+  nEnemies: Math.floor(Math.random() * 50)
+};
+//    Establish axis for board (svg element)
+var axes = {
+  x: d3.scale.linear().domain([0, 100]).range([0, boardSettings.width]),
+  y: d3.scale.linear().domain([0, 100]).range([0, boardSettings.height])
+};
+//    Select container element and append with svg element
+board.append('svg:svg')
+     .attr('width', boardSettings.width)
+     .attr('height', boardSettings.height)
+     .attr('style', 'background: red');
+//    Set board height/width attributes on svg element 
+// Having board, we can create enemies
+//    create random coordinates (x, y) for each enemy
+//    connect coordinates to the enemy objects
+//    render enemies on gameboard 
