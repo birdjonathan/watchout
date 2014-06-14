5b033b7c93a329ac9888743fff17c0ca1c6123fb
Jonathan Bird <jonbird81@gmail.com>
2014-06-13 21:31:04 -0500

    Create and render enemies functions working

--- watchout.js
+++ watchout.js
@@ -1,14 +1,17 @@
 // start slingin' some d3 here.
-// Create the gameboard
-//    Add html element to body for containing gameboard
-var board = d3.select('body');
-//    Add css to position containing element?
-//    Settings for the height, width, set number of enemies (random)
 var boardSettings = {
   height: 450,
   width: 700,
-  nEnemies: Math.floor(Math.random() * 50)
+  nEnemies: 30,
+  padding: 20
 };
+// Create the gameboard
+//    Add html element to body for containing gameboard
+var board = d3.select('.container').append('svg:svg')
+     .attr('width', boardSettings.width)
+     .attr('height', boardSettings.height);
+//    Add css to position containing element?
+//    Settings for the height, width, set number of enemies (random)
 //    Establish axis for board (svg element)
 var axes = {
   x: d3.scale.linear().domain([0, 100]).range([0, boardSettings.width]),
@@ -16,10 +19,7 @@ var axes = {
 };
 //    Select container element and append with svg element
 //    Set board height/width attributes on svg element 
-board.append('svg:svg')
-     .attr('width', boardSettings.width)
-     .attr('height', boardSettings.height)
-     .attr('style', 'background: red');
+//   .attr('style', 'background: red');
 // Having board, we can create enemies
 //    create random coordinates (x, y) for each enemy
 //    connect coordinates to the enemy objects
@@ -33,4 +33,32 @@ var createEnemies = function() {
   });
 };
 //    render enemies on gameboard 
+var renderEnemies = function (enemy_data){
+//      create a construct in memory (enemy data model) for selecting all enemy SVG elements
+  var enemies = board.selectAll('circle.enemy')
+                      .data(enemy_data, function(d){
+                        return d.id;
+                      });
+//          Update DOM with data from enemy model, Attach data to each of those elements- Data is position in coordinates  
+//             Iterate over all the enemy elements set the coordinate attributes with scale coordinate values
+  enemies.enter()
+  .append('svg:circle')
+  .attr('class', 'enemy')
+  .attr('cx', function(enemy){
+      return axes.x(enemy.x);
+  })
+  .attr('cy', function(enemy){
+      return axes.y(enemy.y);
+  })
+  .attr('r', 7);
+
+//                Remove enemies that are no longer in list
+  enemies.exit()
+    .remove();
+  
+}
+
+
+// 
+renderEnemies(createEnemies());
 console.log(createEnemies());
